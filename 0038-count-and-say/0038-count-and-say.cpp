00da352835bf7f1e19bc402f79ebbc2157864533
NIRANJAN V <163496456+Niranjanv2004@users.noreply.github.com>
2025-04-18 13:45:05 +0530

    0038-COUNT-AND-SAY Stats: Time: 0 ms (100%), Space: 9.8 MB (82.8%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     string countAndSay(int n) {
+
         string result = "1";
-        string temp;
+
         for (int i = 1; i < n; i++) {
+            stringstream temp;
             int idx = 0;
+
             while (idx < result.size()) {
                 char c = result[idx];
                 int count = 0;
+
                 while (idx < result.size() && result[idx] == c) {
                     count++;
                     idx++;
                 }
-                temp = temp + to_string(count) + c;
+
+                temp << count << c;
             }
-            result = temp;
-            temp = "";
+
+            result = temp.str();
         }
+
         return result;
     }
 };
