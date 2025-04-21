a90c294ce4698e319699b12197cb1581db99ecd4
NIRANJAN V <163496456+Niranjanv2004@users.noreply.github.com>
2025-04-22 00:46:58 +0530

    2249-COUNT-THE-HIDDEN-SEQUENCES Stats: Time: 6 ms (26.61%), Space: 112.6 MB (47.58%) - LeetHub

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     int numberOfArrays(vector<int>& difference, int lower, int upper) {
 
-        int n = difference.size();
         long long maxel = 1, minel = 1, prev = 1, curr;
 
-        for(int c:difference){
-            curr=c+prev;
+        for (int c : difference) {
+            curr = c + prev;
             prev = curr;
             maxel = max(maxel, curr);
             minel = min(minel, curr);
         }
 
         maxel += (lower - minel);
-        return maxel>upper?0:upper - maxel + 1;
-
-
+        return maxel > upper ? 0 : upper - maxel + 1;
     }
 };
