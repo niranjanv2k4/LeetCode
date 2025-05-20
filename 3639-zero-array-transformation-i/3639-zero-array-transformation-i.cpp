7650a1531fbe107738e52294d5dd80725a5d7a5d
NIRANJAN V <163496456+niranjanv2k4@users.noreply.github.com>
2025-05-20 12:54:30 +0530

    3639-ZERO-ARRAY-TRANSFORMATION-I Stats: Time: 3 ms (87.3%), Space: 290.2 MB (99.89%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        vector<int> rangeD(nums.size(), 0);
+
         int curr = 0, n = nums.size();
-
+        nums.push_back(0);
+        for(int i = n-1; i>=1; i--)
+            nums[i] -= nums[i-1];
+        
         for (auto& query : queries) {
-            rangeD[query[0]] -= 1;
-            if (query[1] + 1 < nums.size()) {
-                rangeD[query[1] + 1] += 1;
-            }
+            nums[query[0]]--;
+            nums[query[1]+1]++;
         }
 
-        for (int i = 0; i < n; i++) {
-            curr += rangeD[i];
-            nums[i] += curr;
-        }
-        return all_of(nums.begin(), nums.end(), [](int x) { return x <= 0; });
+        for (int i = 0; i < n-1; i++) 
+            nums[i+1] += nums[i];
+            
+        return all_of(nums.begin(), nums.end()-1, [](int x) { return x <= 0; });
     }
 };
