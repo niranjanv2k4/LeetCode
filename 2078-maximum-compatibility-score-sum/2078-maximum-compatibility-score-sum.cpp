108c68224e4ea6f3bc6259618cb14196228c824d
NIRANJAN V <163496456+niranjanv2k4@users.noreply.github.com>
2025-05-24 12:00:52 +0530

    2078-MAXIMUM-COMPATIBILITY-SCORE-SUM Stats: Time: 263 ms (22.86%), Space: 13.3 MB (47.99%) - LeetHub

--- 2078-maximum-compatibility-score-sum/2078-maximum-compatibility-score-sum.cpp
+++ 2078-maximum-compatibility-score-sum/2078-maximum-compatibility-score-sum.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
-
     vector<vector<int>> pre;
+    int dp[1 << 8];
     int m, n;
     int maxCompatibilitySum(vector<vector<int>>& students,
                             vector<vector<int>>& mentors) {
         n = students.size();
         m = students[0].size();
-        vector<int> selected(n, -1);
-        pre = vector<vector<int>> (n, vector<int>(n));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int temp = 0;
-                for (int k = 0; k < m; k++) {
+        vector<bool> selected(n, false);
+        pre = vector<vector<int>>(n, vector<int>(n));
+
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < n; j++) 
+                for (int k = 0; k < m; k++) 
                     if (students[i][k] == mentors[j][k])
-                        temp++;
-                }
-                pre[i][j]=temp;
-            }
-        }
-        int res = 0;
-        //return recurse(students, mentors, 0, selected, 0, res);
-        recurse(students, mentors, 0, selected, 0, res);
-        return res;
+                        pre[i][j]++;
+                
+
+        return recurse(0, selected);
     }
 
-    int recurse(vector<vector<int>>& students, vector<vector<int>>& mentors,
-                 int idx, vector<int>& selected, int curr, int& sum) {
-        if(idx==n){
-            sum = max(sum, curr);
+    int recurse(int idx, vector<bool>& selected) {
+        if (idx == n)
             return 0;
-        }
 
         int maxVal = 0;
         for (int i = idx; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (selected[j] != -1)
+                if (selected[j])
                     continue;
-                selected[j] = idx;
-                maxVal = max(maxVal, recurse(students, mentors, i + 1, selected, curr + pre[i][j], sum))+pre[i][j];
-                selected[j] = -1;
+                selected[j] = true;
+                maxVal = max(maxVal, recurse(i+1, selected) + pre[i][j]);
+                selected[j] = false;
             }
         }
         return maxVal;
