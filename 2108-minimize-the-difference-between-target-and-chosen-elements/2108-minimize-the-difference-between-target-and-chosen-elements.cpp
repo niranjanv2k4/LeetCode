8db962de2438950569efaefcda71ace53f65d5d0
NIRANJAN V <163496456+niranjanv2k4@users.noreply.github.com>
2025-05-28 12:49:44 +0530

    2108-MINIMIZE-THE-DIFFERENCE-BETWEEN-TARGET-AND-CHOSEN-ELEMENTS Stats: Time: 39 ms (96.78%), Space: 17.1 MB (93.56%) - LeetHub

--- 2108-minimize-the-difference-between-target-and-chosen-elements/2108-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 2108-minimize-the-difference-between-target-and-chosen-elements/2108-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     int minimizeTheDifference(vector<vector<int>>& mat, int target) {
-        int val = INT_MAX;
-        vector<vector<bool>> dp(mat.size(), vector<bool>(target+5000, false));
-        int global= INT_MAX;
-        vector<int> minRemaining(mat.size()+1, 0);
-        for(int i = mat.size()-1; i>=0; i--){
-            minRemaining[i] = (*min_element(mat[i].begin(), mat[i].end())) + minRemaining[i+1];
+        bitset<5000> bs;
+        bs[0] = 1;
 
+        for (auto& row : mat) {
+            bitset<5000> new_bs;
+            for (int el : row) {
+                new_bs |= (bs << el);
+            }
+            bs = new_bs;
         }
-        return recurse(mat, 0, target, 0, dp, global, minRemaining);
-    }
 
-    int recurse(vector<vector<int>>& mat, int idx, int target, int curr, vector<vector<bool>>& dp, int& global, vector<int>& minRemaining) {
-        if (idx == mat.size()) {
-            return global = abs(target-curr);
-        }   
-        if(curr>=dp[0].size())return INT_MAX;
-        if(dp[idx][curr])return INT_MAX;
-        dp[idx][curr]=true;
-        // if(dp.count(idx) && dp[idx].count(curr))return dp[idx][curr];
-
-        int minVal = INT_MAX, val;
-        for (int i = 0; i < mat[idx].size(); i++) {
-            val = recurse(mat, idx + 1, target, curr + mat[idx][i], dp, global, minRemaining);
-            minVal = min(val, minVal);
+        int res = INT_MAX;
+        for (int i = 0; i < 4900; i++) {
+            if (bs[i]) {
+                res = min(res, abs(target - i));
+            }
         }
-        return minVal;
+        return res;
     }
 };
