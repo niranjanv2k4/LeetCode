58057bbd1f21ab76e1c62f915f86615037913155
NIRANJAN V <163496456+niranjanv2k4@users.noreply.github.com>
2025-05-29 11:51:23 +0530

    3645-MAXIMIZE-THE-NUMBER-OF-TARGET-NODES-AFTER-CONNECTING-TREES-II Stats: Time: 318 ms (64.63%), Space: 332.2 MB (94.56%) - LeetHub

--- 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
@@ -3,81 +3,48 @@ public:
     vector<int> maxTargetNodes(vector<vector<int>>& edges1,
                                vector<vector<int>>& edges2) {
 
-        const int n = edges1.size() + 1, m = edges2.size() + 1;
-        vector<int> even(n);
+        const int n = edges1.size() + 1;
+        const int m = edges2.size() + 1;
 
         auto adj1 = createAdj(edges1, n);
         auto adj2 = createAdj(edges2, m);
-        auto isEven = levelCheck(adj1);
+        auto [even2, _] = levelCheck(adj2);
+        auto [even1, isEven] = levelCheck(adj1);
 
-        int even2 = totalVertex2(adj2);
+        vector<int> even(n);
         int count = max(even2, m - even2);
-        int even1 = totalVertex2(adj1);
 
-        for (int i = 0; i < n; i++) {
-            if (isEven[i])
-                even[i] = even1 + count;
-            else
-                even[i] = n - even1 + count;
-        }
+        for (int i = 0; i < n; i++)
+            even[i] = (isEven[i] ? even1 : n - even1) + count;
+
         return even;
     }
 
-    int totalVertex2(vector<vector<int>>& adj) {
-
-        vector<bool> visited(adj.size(), false);
-        queue<int> q;
-        int count = 0;
-        bool flag = true;
-        q.push(0);
-        visited[0] = true;
-
-        while (!q.empty()) {
-            int size = q.size();
-            while (size--) {
-                int node = q.front();
-                if (flag)
-                    count++;
-                q.pop();
-                for (int neighbor : adj[node]) {
-                    if (!visited[neighbor]) {
-                        visited[neighbor] = true;
-                        q.push(neighbor);
-                    }
-                }
-            }
-            flag = !flag;
-        }
-        return count;
-    }
-    vector<bool> levelCheck(vector<vector<int>>& adj) {
+    pair<int, vector<bool>> levelCheck(vector<vector<int>>& adj) {
 
         vector<bool> visited(adj.size(), false);
         vector<bool> isEven(adj.size(), false);
-        queue<int> q;
         bool flag = true;
+        queue<int> q;
+        int count = 0;
 
         q.push(0);
-        visited[0] = true;
-        isEven[0] = true;
+        visited[0] = isEven[0] = true;
 
         while (!q.empty()) {
-            int size = q.size();
-            while (size--) {
-                int node = q.front();
-                if (flag)
-                    isEven[node] = true;
-                q.pop();
-                for (int neighbor : adj[node]) {
-                    if (!visited[neighbor]) {
-                        visited[neighbor] = true;
-                        q.push(neighbor);
-                    }
+            int node = q.front();
+            if (isEven[node])
+                count++;
+            q.pop();
+            for (int neighbor : adj[node]) {
+                if (!visited[neighbor]) {
+                    isEven[neighbor] = !isEven[node];
+                    visited[neighbor] = true;
+                    q.push(neighbor);
                 }
             }
-            flag = !flag;
         }
-        return isEven;
+        return {count, isEven};
     }
 
     vector<vector<int>> createAdj(vector<vector<int>>& edges, int n) {
