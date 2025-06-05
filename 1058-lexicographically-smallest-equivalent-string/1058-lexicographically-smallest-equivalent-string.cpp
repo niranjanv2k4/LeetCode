a98052400a96a89f23c97db337386681b7773d6b
NIRANJAN V <163496456+niranjanv2k4@users.noreply.github.com>
2025-06-05 10:16:20 +0530

    1058-LEXICOGRAPHICALLY-SMALLEST-EQUIVALENT-STRING Stats: Time: 0 ms (100%), Space: 8.8 MB (85.16%) - LeetHub

--- 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.cpp
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.cpp
@@ -1,40 +1,26 @@
 class Solution {
 public:
     string smallestEquivalentString(string s1, string s2, string baseStr) {
-        vector<vector<int>> adj(26);
-        vector<char> lowest(26, 0);
+        vector<int> groups(26, -1);
         for (int i = 0; i < s1.size(); i++) {
-            adj[s1[i] - 'a'].push_back(s2[i] - 'a');
-            adj[s2[i] - 'a'].push_back(s1[i] - 'a');
+            int one = find(groups, s1[i] - 'a');
+            int two = find(groups, s2[i] - 'a');
+            if (one != two) {
+                if (two < one) {
+                    groups[one] = two;
+                } else
+                    groups[two] = one;
+            }
         }
         string res = "";
-        for (char c : baseStr) {
-            int curr = c - 'a';
-            if (lowest[curr] == 0)
-                res += bfs(adj, curr, lowest);
-            else
-                res += lowest[curr];
-        }
+        for (char c : baseStr)
+            res += find(groups, c - 'a') + 'a';
         return res;
     }
 
-    char bfs(vector<vector<int>> &adj, int start, vector<char> &lowest) {
-        vector<bool> visited(adj.size(), false);
-        visited[start] = true;
-        queue<int> q;
-        q.push(start);
-        int curr = start;
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            curr = curr < node ? curr : node;
-            for (auto neighbor : adj[node]) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    q.push(neighbor);
-                }
-            }
-        }
-        return lowest[start] = curr + 'a';
+    int find(vector<int>& groups, int c) {
+        if (groups[c] == -1)
+            return c;
+        return groups[c] = find(groups, groups[c]);
     }
 };
