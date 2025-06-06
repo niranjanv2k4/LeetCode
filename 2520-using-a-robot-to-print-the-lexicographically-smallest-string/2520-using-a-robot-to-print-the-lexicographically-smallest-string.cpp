c912fe2751cdb2a706909c68cc4d6132767b57e6
NIRANJAN V <163496456+niranjanv2k4@users.noreply.github.com>
2025-06-06 15:19:13 +0530

    2520-USING-A-ROBOT-TO-PRINT-THE-LEXICOGRAPHICALLY-SMALLEST-STRING Stats: Time: 22 ms (98.02%), Space: 34.2 MB (54.95%) - LeetHub

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -3,9 +3,8 @@ public:
     string robotWithString(string s) {
 
         const int n = s.size();
-        stack<int> st;
+        string st = "", res = "";
         vector<char> arr(n);
-        string res = "";
 
         arr[n - 1] = s[n - 1];
         for (int i = n - 2; i >= 0; i--) {
@@ -13,17 +12,15 @@ public:
         }
 
         for (int i = 0; i < n; i++) {
-            while (!st.empty() && st.top() <= arr[i]) {
-                res += st.top();
-                st.pop();
+            while (st.size() != 0 && st.back() <= arr[i]) {
+                res += st.back();
+                st.pop_back();
             }
-            st.push(s[i]);
+            st.push_back(s[i]);
         }
 
-        while (!st.empty()) {
-            res += st.top();
-            st.pop();
-        }
+        reverse(st.begin(), st.end());
+        res += st;
 
         return res;
     }
