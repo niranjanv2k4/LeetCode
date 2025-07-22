e95f599c8a7a1d6bc9506784fe12bcf8a6563129
NIRANJAN V <163496456+niranjanv2k4@users.noreply.github.com>
2025-07-22 21:12:48 +0530

    0130-SURROUNDED-REGIONS Stats: Time: 1 ms (35.83%), Space: 15.3 MB (22.21%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -3,34 +3,8 @@ public:
     void solve(vector<vector<char>>& board) {
         int n = board.size(), m = board[0].size();
 
-        for (int i = 1; i < board.size() - 1; i++) {
-            for (int j = 1; j < board[0].size() - 1; j++) {
-                if (board[i][j] == 'O')
-                    bfs(board, i, j);
-            }
-        }
-    }
-
-    void update(vector<vector<char>>& board, vector<vector<bool>>& visited) {
-        for (int i = 1; i < visited.size() - 1; i++) {
-            for (int j = 1; j < visited[0].size() - 1; j++) {
-                if (visited[i][j]) {
-                    board[i][j] = 'X';
-                }
-            }
-        }
-    }
-
-    void bfs(vector<vector<char>>& board, int r, int c) {
-        int n = board.size(), m = board[0].size();
-
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
         vector<int> dir = {0, 1, 0, -1, 0};
-        queue<pair<int, int>> q;
-
-        q.push({r, c});
-        visited[r][c] = true;
-        bool res = true;
+        auto [q, visited] = create_queue(board);
 
         while (!q.empty()) {
             auto [r, c] = q.front();
@@ -42,9 +16,6 @@ public:
                     continue;
 
                 if (board[nr][nc] == 'O') {
-                    if ((nr == 0 || nc == 0 || nr == n - 1 || nc == m - 1)) {
-                        res = false;
-                    }
                     if (!visited[nr][nc]) {
                         visited[nr][nc] = true;
                         q.push({nr, nc});
@@ -52,8 +23,45 @@ public:
                 }
             }
         }
+        
+        update(board, visited);
+    }
 
-        if (res)
-            update(board, visited);
+    void update(vector<vector<char>>& board, vector<vector<bool>>& visited) {
+        for (int i = 1; i < visited.size() - 1; i++) {
+            for (int j = 1; j < visited[0].size() - 1; j++) {
+                if (!visited[i][j] && board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    pair<queue<pair<int, int>>, vector<vector<bool>>>
+    create_queue(vector<vector<char>>& board) {
+        int n = board.size(), m = board[0].size();
+        queue<pair<int, int>> q;
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        for (int i = 0; i < n; i++) {
+            if (board[i][0] == 'O') {
+                q.push({i, 0});
+                visited[i][0] = true;
+            }
+            if (board[i][m - 1] == 'O') {
+                q.push({i, m - 1});
+                visited[i][m - 1] = true;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            if (board[0][i] == 'O') {
+                q.push({0, i});
+                visited[0][i] = true;
+            }
+            if (board[n - 1][i] == 'O') {
+                q.push({n - 1, i});
+                visited[n - 1][i] = true;
+            }
+        }
+        return {q, visited};
     }
 };
