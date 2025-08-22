bb2ccb91fb1162cd427a80bad6d05a43dfbca975
NIRANJAN V <163496456+niranjanv2k4@users.noreply.github.com>
2025-08-22 19:09:40 +0530

    3461-FIND-THE-MINIMUM-AREA-TO-COVER-ALL-ONES-I Stats: Time: 277 ms (13.1%), Space: 133.5 MB (38.34%) - LeetHub

--- 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     int minimumArea(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<int> row(n, 0), col(m, 0);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        
+        int r = 0, d = 0, l = INT_MAX, u = INT_MAX;
+
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
                 if (grid[i][j] == 1) {
-                    row[i]++;
-                    col[j]++;
+                    u = min(u, i);
+                    d = max(d, i);
+
+                    l = min(l, j);
+                    r = max(r, j);
                 }
             }
         }
 
-        int r = 0, d = 0, l = INT_MAX, u = INT_MAX;
 
-        for (int i = 0; i < m; i++) {
-            if (col[i] != 0) {
-                r = i;
-                l = min(i, l);
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            if (row[i] != 0) {
-                d = i;
-                u = min(i, u);
-            }
-        }
         return (r - l + 1) * (d - u + 1);
     }
 };
