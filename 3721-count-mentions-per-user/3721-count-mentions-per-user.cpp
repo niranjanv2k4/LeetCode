57613db0e7ccbeb625696dd2f5953719bd25a7ee
NIRANJAN V <163496456+niranjanv2k4@users.noreply.github.com>
2025-12-12 13:03:34 +0530

    3721-COUNT-MENTIONS-PER-USER Stats: Time: 739 ms (5.67%), Space: 43.4 MB (60.99%) - LeetHub

--- 3721-count-mentions-per-user/3721-count-mentions-per-user.cpp
+++ 3721-count-mentions-per-user/3721-count-mentions-per-user.cpp
@@ -3,63 +3,84 @@ public:
     vector<int> countMentions(int numberOfUsers,
                               vector<vector<string>>& events) {
 
-        sort(events.begin(), events.end(),
-             [&](const vector<string>& a, const vector<string>& b) {
-                 int at = stoi(a[1]);
-                 int bt = stoi(b[1]);
-                 if (at != bt) {
-                     return at < bt;
-                 }
-                 if (b[0] == "OFFLINE") {
-                     return false;
-                 }
-                 return true;
-             });
+        stable_sort(events.begin(), events.end(),
+                    [](const vector<string>& a, const vector<string>& b) {
+                        int ta = stoi(a[1]);
+                        int tb = stoi(b[1]);
 
-        vector<int> res(numberOfUsers, 0), status(numberOfUsers, 0);
+                        if (ta != tb)
+                            return ta < tb;
 
-        for (auto& event : events) {
-            string type = event[0];
-            int curr = stoi(event[1]);
-            string users = event[2];
+                        bool aOff = (a[0] == "OFFLINE");
+                        bool bOff = (b[0] == "OFFLINE");
 
-            if (type == "OFFLINE") {
-                status[extractVal(users)] = curr + 60;
-            } else {
+                        return aOff && !bOff;
+                    });
 
-                if (users == "ALL") {
-                    for (int id = 0; id < numberOfUsers; id++) {
-                        res[id]++;
-                    }
+        vector<int> res(numberOfUsers, 0);
+        unordered_map<int, priority_queue<int, vector<int>, greater<int>>> mp;
 
-                } else if (users == "HERE") {
-                    for (int id = 0; id < numberOfUsers; id++) {
-                        if (status[id] <= curr) {
-                            res[id]++;
-                        }
-                    }
+        int n = events.size();
+        int i = 0;
 
-                } else {
-                    stringstream ss(users);
-                    string user;
+        for (int time = stoi(events[0][1]);
+             i < n && time <= stoi(events[n - 1][1]); time++) {
+
+            vector<int> del;
+            for (auto& p : mp) {
+                auto& pq = p.second;
+                while (!pq.empty() && pq.top() <= time)
+                    pq.pop();
+                if (pq.empty())
+                    del.push_back(p.first);
+            }
+            for (int id : del)
+                mp.erase(id);
+
+            while (i < n && stoi(events[i][1]) == time) {
+
+                stringstream ss(events[i][2]);
+                string user;
+
+                if (events[i][0] == "OFFLINE") {
 
                     while (ss >> user) {
-                        res[extractVal(user)]++;
+                        int id = extractVal(user);
+                        mp[id].push(time + 60);
+                    }
+
+                } else if (events[i][2] == "ALL") {
+
+                    for (int id = 0; id < numberOfUsers; id++)
+                        res[id]++;
+
+                } else if (events[i][2] == "HERE") {
+
+                    for (int id = 0; id < numberOfUsers; id++)
+                        if (mp.find(id) == mp.end())
+                            res[id]++;
+
+                } else {
+
+                    while (ss >> user) {
+                        int id = extractVal(user);
+                        // if (mp.find(id) == mp.end())
+                            res[id]++;
                     }
                 }
+
+                i++;
             }
         }
 
         return res;
     }
 
-    int extractVal(const string& s) {
+    int extractVal(string s) {
         int id = 0;
-        for (int i = 0; i < s.length(); ++i) {
-            if (isdigit(s[i])) {
-                id = id * 10 + (s[i] - '0');
-            }
-        }
+        for (char c : s)
+            if (isdigit(c))
+                id = id * 10 + (c - '0');
         return id;
     }
 };
