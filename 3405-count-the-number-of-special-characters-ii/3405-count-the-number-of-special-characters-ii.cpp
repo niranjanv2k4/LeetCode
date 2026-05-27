1c16e8344da0755cbcf4e6423ffb925f31893cb4
NIRANJAN V <163496456+niranjanv2k4@users.noreply.github.com>
2026-05-27 08:22:23 +0530

    3405-COUNT-THE-NUMBER-OF-SPECIAL-CHARACTERS-II Stats: Time: 88 ms (30.34%), Space: 25.5 MB (29.21%) - LeetHub

--- 3405-count-the-number-of-special-characters-ii/3405-count-the-number-of-special-characters-ii.cpp
+++ 3405-count-the-number-of-special-characters-ii/3405-count-the-number-of-special-characters-ii.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        unordered_map<char, int> mp;
+        
+        unordered_map<char, int> lower_index, upper_index;
         int res = 0;
 
-        for (char ch : word)
-            mp[ch]++;
-
-        for (char ch : word) {
-            char lower_ch = tolower(ch);
-            
-            if (isupper(ch) && mp.find(lower_ch) != mp.end()) {
-                if (mp[lower_ch] == 0)
-                    res++;
-                mp.erase(lower_ch);
+        for(int i = 0; i<word.size(); i++){
+            if(isupper(word[i])){
+                if(upper_index.find(word[i]) == upper_index.end())
+                    upper_index[word[i]] = i;
+            }else{
+                lower_index[word[i]] = i;
             }
+        }
 
-            if (islower(ch))
-                mp[ch]--;
+        for(auto [ch, idx] : lower_index){
+            if(upper_index.find(toupper(ch)) != upper_index.end() && upper_index[toupper(ch)] > idx)
+                res++;
         }
 
         return res;
